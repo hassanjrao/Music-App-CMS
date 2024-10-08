b0a28869735ce43307336f20316b846c06737e9e
Hassan Javaid <hassanjrao@gmail.com>
2024-10-08 21:56:24 +0400

    dashboair issue

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -246,7 +246,7 @@
 
                         <li class="nav-main-item">
                             <a class="nav-main-link{{ request()->is('dashboard') ? ' active' : '' }}  {{ request()->is('/') ? ' active' : '' }}"
-                                href="/dashboard">
+                                href="{{ route('dashboard') }}">
                                 <i class="nav-main-link-icon si si-cursor"></i>
                                 <span class="nav-main-link-name">Dashboard</span>
                             </a>
